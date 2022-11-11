36da4d54d89e30fd692fa1acf900355ec349e373
Roman Tsykalo <u15921@mail.ru>
2022-11-11 08:35:29 +0400

    DZ Close

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message + "->");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = ReadInt("Введите первое число");
+int number2 = ReadInt("Введите второе число");
+
+int[] numbers = new int[number2];
+
+
+for(int i = 0; i < number2; i++)
+{
+    numbers[i] = number;
+}
+
+int result = 1;
+
+for(int i = 0; i < number2; i++)
+{
+result = result * numbers[i];
+}
+
+Console.Write(result);
